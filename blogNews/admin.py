babaee477e94f6a35987bcdeff7536e628d9726e
laranda <luisaranda657@gmail.com>
2019-03-28 09:52:46 +0100

    Model data and panel administration without constraints

--- blogNews/admin.py
+++ blogNews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import New
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(New)
+admin.site.unregister(User)
+
+class MyUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')}
+        ),
+    )
+
+admin.site.register(User, MyUserAdmin)
--- blogNews/models.py
+++ blogNews/models.py
@@ -3,18 +3,21 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+class BaseItems(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
+    subtitle = models.CharField(max_length=200)
+    body = models.TextField()
+    class Meta:
+        abstract=True
+
+    def __str__(self):
+        return self.title
+
+class New(BaseItems):
     published_date = models.DateTimeField(
-            blank=True, null=True)
+        blank=True, null=True)
+    image = models.ImageField(upload_to='news')
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
-    def __str__(self):
-        return self.title
