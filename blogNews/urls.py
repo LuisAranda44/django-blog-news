19e4ab388fc3f5b892c3a5683b442160810a3df7
laranda <luisaranda657@gmail.com>
2019-04-05 10:44:01 +0200

    Added Class-Based Views to my blogNews model

--- blogNews/urls.py
+++ blogNews/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from . import views
 
 urlpatterns = [
-    path('', views.new_list, name='new_list'),
+    path('v1', views.new_list, name='new_list'),
     path('v1/news/<int:pk>/', views.new_detail, name='new_detail'),
     path('v1/news/create',views.new_add, name="new_add"),
     path('v1/news/<int:pk>/edit/', views.new_edit, name='new_edit'),
+    path('v1/news/<int:pk>/delete/', views.new_delete, name='new_delete'),
+    path('v2',views.NewList.as_view(), name = "new_list_class"),
+    path('v2/news/<int:pk>/', views.NewDetail.as_view(), name='new_detail_class'),
+    path('v2/news/create/',views.NewCreate.as_view(), name="new_add_class"),
+    path('v2/news/<int:pk>/edit/', views.NewUpdate.as_view(), name="new_edit_class"),
+    path('v2/news/<int:pk>/delete/', views.NewDelete.as_view(), name='new_delete'),
 ]
--- blogNews/views.py
+++ blogNews/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+
 from .models import New
 from .forms import NewForm
-from django.shortcuts import redirect
+
 
 # Create your views here.
+#Function Based Views
 def new_list(request):
     news = New.objects.all().order_by('-published_date')
     return render(request, 'blogNews/new_list.html', {'news':news})
@@ -33,4 +38,38 @@ def new_edit(request,pk):
         form = NewForm(instance=new)
     return render(request, 'blogNews/new_edit.html', {'form': form})
 
+def new_delete(request, pk):
+    new = get_object_or_404(New, pk=pk)
+    if request.method == "POST":
+        new.delete()
+        return redirect('new_list',)
+    return render(request, 'blogNews/new_delete.html', {'new': new})
 
+#Class Based Views
+class NewList(ListView):
+    model = New
+    template_name = 'blogNews/new_list_class.html'
+    context_object_name = 'news'
+
+class NewDetail(DetailView):
+    #model = New
+    template_name= 'blogNews/new_detail_class.html'
+    queryset = New.objects.all()
+
+class NewCreate(CreateView):
+    model = New
+    form_class = NewForm
+    template_name= 'blogNews/new_edit_class.html'
+    success_url = reverse_lazy('new_list_class')
+
+class NewUpdate(UpdateView):
+    model = New
+    form_class = NewForm
+    template_name = 'blogNews/new_edit_class.html'
+    success_url = reverse_lazy('new_list_class')
+
+
+class NewDelete(DeleteView):
+    model = New
+    template_name = 'blogNews/new_delete.html'
+    success_url = reverse_lazy('new_list_class')
